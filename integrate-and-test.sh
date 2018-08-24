9054e63e3c9f4930863828fbc92eb52312d4d6ea
Callum Stott <callum@seadowg.com>
2018-08-24 13:36:57 +0100

    Add test scripts back in so single and multi process can co exist

---
+++ integrate-and-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+pushd api
+  bundle exec rake db:create db:migrate
+  bundle exec rake
+popd
+
+pushd web
+  gulp spec-app
+popd
+
+./integrate-web-into-api.sh
+
+pushd web
+  gulp mock-google-auth-server &
+  MOCK_PID=$!
+  trap 'kill -9 $MOCK_PID' EXIT
+  sleep 20
+popd
+
+pushd api
+  bundle exec rake db:create db:migrate db:test:prepare
+  RAILS_ENV=test bundle exec rake db:seed
+  GOOGLE_AUTH_ENDPOINT=http://localhost:3100/auth bundle exec rails server -e test &
+  RAILS_PID=$!
+  trap 'kill -9 $RAILS_PID' EXIT
+  sleep 10
+popd
+
+pushd e2e
+  bundle exec rspec spec
+popd
--- test.sh
+++ test.sh
@@ -9,26 +9,5 @@ popd
 
 pushd web
   gulp spec-app
-popd
-
-./integrate-web-into-api.sh
-
-pushd web
-  gulp mock-google-auth-server &
-  MOCK_PID=$!
-  trap 'kill -9 $MOCK_PID' EXIT
-  sleep 20
-popd
-
-pushd api
-  bundle exec rake db:create db:migrate db:test:prepare
-  RAILS_ENV=test bundle exec rake db:seed
-  GOOGLE_AUTH_ENDPOINT=http://localhost:3100/auth bundle exec rails server -e test &
-  RAILS_PID=$!
-  trap 'kill -9 $RAILS_PID' EXIT
-  sleep 10
-popd
-
-pushd e2e
-  bundle exec rspec spec
+  gulp local-acceptance
 popd
